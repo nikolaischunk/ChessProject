dde6d2595ebadd798112704d16bf906d1cf79cc1
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-09-24 09:23:32 +0200

    Update KingTest.java

--- src/Test/KingTest.java
+++ src/Test/KingTest.java
@@ -2,10 +2,12 @@ package Test;
 
 import Chess.Color;
 import Chess.King;
+import Chess.Queen;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 
 class KingTest {
@@ -25,5 +27,17 @@ class KingTest {
         Point targetLocation = new Point(4, 7);
         Assertions.assertThat(king.canMove(currentLocation, targetLocation)).isEqualTo(true);
     }
-    
+
+//    @Test
+//    void getKingMovingPath(){
+//        King king = new King(Color.BLACK);
+//        ArrayList<Point> movingPath = new ArrayList<>();
+//        Point currentLocation = new Point(4, 1);
+//        Point targetLocation = new Point(4, 2);
+//        movingPath.add(currentLocation);
+//        movingPath.add(currentLocation);
+//        Assertions.assertThat(king.getMovingPath(currentLocation, targetLocation)).size().isEqualsTo(getKingMovingPath());
+//
+//    }
+
 }
